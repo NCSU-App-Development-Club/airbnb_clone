663ab986b294795b1efc3a7dbb264b094bec531e
Rohan-Nandula <rohankausikn@gmail.com>
2024-09-08 17:46:23 -0400

    Initial Commit

---
+++ airbnbclone/airbnbcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbcloneApp.swift
+//  airbnbclone
+//
+//  Created by Rohan Nandula on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct airbnbcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
