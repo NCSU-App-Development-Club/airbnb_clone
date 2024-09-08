5b0ae32a7c2d0b52afce849d4cc0f23dbbc59022
Rohan-Nandula <rohankausikn@gmail.com>
2024-09-08 18:32:01 -0400

    Add padding

--- airbnbclone/ContentView.swift
+++ airbnbclone/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
                 .fontWeight(.semibold)
                 .font(.title)
+                .padding()
         }
         .padding()
     }
